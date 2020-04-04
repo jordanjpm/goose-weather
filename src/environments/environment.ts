d896de965a757b275da3f86ebd28d32109a63a4f
Jordan Peralta Moscoso <jordanjpm@gmail.com>
2020-04-04 11:46:50 -0400

    update env script

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  openWeatherMapAPIKey: 'OPEN_WEATHER_MAP_API_KEY',
+  openWeatherMapAPIKey: '$OPEN_WEATHER_MAP_API_KEY',
   noaaMetaDataEndpoint: 'https://api.weather.gov/points/'
 };
 

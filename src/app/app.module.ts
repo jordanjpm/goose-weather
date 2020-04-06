9e52d31a9599d83ae30108fe44166ae87b74b114
Jordan Peralta Moscoso <jordanjpm@gmail.com>
2020-04-05 20:55:01 -0400

    refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,58 +1,48 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AngularMaterialModule } from '../angular-material/angular-material.module';
-import { LayoutModule } from '@angular/cdk/layout';
-import { WeatherComponent } from './weather/weather.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { CurrentConditionsComponent } from './cards/current-conditions/current-conditions.component';
-import { WeatherDiscussionComponent } from './cards/weather-discussion/weather-discussion.component';
-import { HttpClientModule } from '@angular/common/http';
-import { WeeklyForecastComponent } from './cards/weekly-forecast/weekly-forecast.component';
-import { HourlyForecastComponent } from './cards/hourly-forecast/hourly-forecast.component';
-import { NgxdModule } from '@ngxd/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './store/reducers';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
-import { EffectsModule } from '@ngrx/effects';
-import { WeatherEffects } from './store/effects/weather.effects';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxdModule } from '@ngxd/core';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AngularMaterialModule } from './angular-material.module';
+
+import { AppComponent } from './app.component';
+import { CurrentConditionsComponent } from './cards/current-conditions/current-conditions.component';
+import { HourlyForecastComponent } from './cards/hourly-forecast/hourly-forecast.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { WeatherComponent } from './weather/weather.component';
+import { WeatherDiscussionComponent } from './cards/weather-discussion/weather-discussion.component';
+import { WeeklyForecastComponent } from './cards/weekly-forecast/weekly-forecast.component';
+
+import { reducers, metaReducers } from './store/reducers';
+import { WeatherEffects } from './store/effects/weather.effects';
+
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WeatherComponent,
+    CurrentConditionsComponent,
+    HourlyForecastComponent,
     PageNotFoundComponent,
-    CurrentConditionsComponent,
+    WeatherComponent,
     WeatherDiscussionComponent,
-    WeeklyForecastComponent,
-    HourlyForecastComponent,
-  ],
-  entryComponents: [
-    CurrentConditionsComponent,
-    WeatherDiscussionComponent,
-    WeeklyForecastComponent,
-    HourlyForecastComponent,
+    WeeklyForecastComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     AngularMaterialModule,
-    LayoutModule,
     HttpClientModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    NgxdModule,
     FormsModule,
     ReactiveFormsModule,
+    NgxdModule,
     StoreModule.forRoot(reducers, {
       metaReducers,
       runtimeChecks: {
